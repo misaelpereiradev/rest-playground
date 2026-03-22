90500de1fa21d5a195edfd1ec36eeed04dc3410e
Misael Pereira <misael.pereira.dev@outlook.com>
2026-03-22 12:09:28 +0100

    chore: init project

---
+++ src/main/java/com/misaelpereiradev/rest_playground/RestPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.misaelpereiradev.rest_playground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestPlaygroundApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(RestPlaygroundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/misaelpereiradev/rest_playground/RestPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.misaelpereiradev.rest_playground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestPlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
